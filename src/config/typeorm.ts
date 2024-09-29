7b067c7bdbcfad3f8ea6ef30e8325c57de431565
Zulfikar Fajri <fajrizulfikar85@gmail.com>
2024-09-30 05:25:49 +0700

    feat: migration setup

---
+++ src/config/typeorm.ts
@@ -0,0 +1,19 @@
+import { config as dotenvConfig } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+dotenvConfig({ path: '.env' });
+
+const config = {
+  type: 'mysql',
+  host: `localhost`,
+  port: 3306,
+  username: `${process.env.DB_USERNAME}`,
+  password: `${process.env.DB_PASSWORD}`,
+  database: `${process.env.DB_NAME}`,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  autoLoadEntities: true,
+  synchronize: false,
+};
+
+export default new DataSource(config as DataSourceOptions);
