328523ad41d0c66eac3607f1e375e3b6a14e7c9f
Zulfikar Fajri <zulfikar.fajri@admo.astra.co.id>
2024-10-02 09:28:24 +0700

    feat: init rmq

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FirebaseModule } from './firebase/firebase.module';
 import { DatabaseModule } from './database/database.module';
+import { RmqModule } from './rmq/rmq.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ cache: true, isGlobal: true }),
-    FirebaseModule,
+    // FirebaseModule,
     DatabaseModule,
+    RmqModule.register({ name: 'NOTIFICATION_SERVICE' }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RmqService } from './rmq/rmq.service';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const rmqService = app.get<RmqService>(RmqService);
+
+  app.connectMicroservice(rmqService.getOptions('notification.fcm', true));
+
+  await app.startAllMicroservices();
   await app.listen(3000);
 }
 bootstrap();
